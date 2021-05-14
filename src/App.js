9a51e59d93307360d6df1b01ecdbd707f964f1e5
Sofia <sofigava@gmail.com>
2021-05-13 19:21:04 -0500

    priemr commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state={
+      count:0
+    }
+   // this.updateCounter = this.updateCounter.bind(this);
+  }
+
+  updateCounter = () => {
+    this.setState({count: this.state.count+1});  
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={this.updateCounter}>Incrementa</button>
       </div>
     );
   }
+
+
 }
 
 export default App;
